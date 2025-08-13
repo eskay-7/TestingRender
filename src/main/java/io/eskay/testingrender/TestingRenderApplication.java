3eb3a749ec4021ae634c25364d0a15b53b2e83fa
Seth Ofosu Okyere <eskay.dev7@gmail.com>
2025-08-13 08:38:40 +0000

    initial commit with first endpoint

---
+++ src/main/java/io/eskay/testingrender/TestingRenderApplication.java
@@ -0,0 +1,22 @@
+package io.eskay.testingrender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@SpringBootApplication
+public class TestingRenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingRenderApplication.class, args);
+    }
+
+    @GetMapping("/one")
+    public String getOne() {
+        return "This is the first endpoint";
+    }
+}
---
+++ src/test/java/io/eskay/testingrender/TestingRenderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.eskay.testingrender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingRenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
